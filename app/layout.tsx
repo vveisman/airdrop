a816bd8fbff41797f7906e9f136cbaaa0f50dc8c
vveisman <153898369+vveisman@users.noreply.github.com>
2024-01-17 21:28:04 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Open_Sans, Abhaya_Libre } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/react";
 
 // const inter = Inter({ subsets: ["latin"] });
 const inter = Open_Sans({ subsets: ["latin"] });
@@ -26,7 +27,10 @@ export default function RootLayout({
         className={`bg-gradient-to-r relative from-[#2e2c46ff] to-[#373461ff] ${inter.className}`}
       >
         <Navbar />
-        <div className='w-full'>{children}</div>
+        <div className='w-full'>
+          {children}
+          <Analytics />
+        </div>
       </body>
     </html>
   );
